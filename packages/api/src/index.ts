4ce0096438737206655d4030f748c77d7aacbc6b
Matthias Van Parijs <matvp91@gmail.com>
2024-10-30 00:24:37 +0100

    Removed unused code

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,7 +1,6 @@
 import { Elysia, t } from "elysia";
 import { cors } from "@elysiajs/cors";
 import { swagger } from "@matvp91/elysia-swagger";
-import { jwt } from "@elysiajs/jwt";
 import { env } from "./env";
 import { auth } from "./routes/auth";
 import { jobs } from "./routes/jobs";
