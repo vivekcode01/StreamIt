6c44b8cacc0200ee5633cb2d6842be27fe2413d4
Matthias Van Parijs <matvp91@gmail.com>
2024-10-30 11:50:13 +0100

    Stop auto migrating

---
+++ packages/api/src/db/migrate.ts
@@ -0,0 +1,42 @@
+import { Migrator, FileMigrationProvider } from "kysely";
+import * as path from "node:path";
+import * as fs from "node:fs/promises";
+import { db } from ".";
+
+/*
+DROP SCHEMA public CASCADE;
+CREATE SCHEMA public;
+*/
+
+async function migrateToLatest() {
+  const migrator = new Migrator({
+    db,
+    provider: new FileMigrationProvider({
+      fs,
+      path,
+      migrationFolder: path.join(__dirname, "migrations"),
+    }),
+  });
+
+  const { error, results } = await migrator.migrateToLatest();
+
+  results?.forEach((it) => {
+    if (it.status === "Success") {
+      console.log(`Migration "${it.migrationName}" was executed successfully`);
+    } else if (it.status === "Error") {
+      console.error(`Failed to execute migration "${it.migrationName}"`);
+    }
+  });
+
+  if (error) {
+    console.error("Failed to migrate");
+    console.error(error);
+    process.exit(1);
+  }
+
+  console.log(`Ran ${results?.length ?? 0} migrations`);
+
+  await db.destroy();
+}
+
+migrateToLatest();
