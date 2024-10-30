8a52e96d5ab4c78dccfeb53f88cb5b007c6c65a3
Matthias Van Parijs <matvp91@gmail.com>
2024-10-30 14:43:16 +0100

    Removed unused code

--- packages/shared/src/jwt.ts
+++
@@ -1,8 +0,0 @@
-import { SignJWT } from "jose";
-
-export async function createJwtServiceToken(secret: string) {
-  const jwt = new SignJWT({
-    type: "service",
-  }).setProtectedHeader({ alg: "HS256" });
-  return await jwt.sign(new TextEncoder().encode(secret));
-}
