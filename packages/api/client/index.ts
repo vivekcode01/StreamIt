7f0b01e935634ed24da8e5c07c783feff10825ec
Matthias Van Parijs <matvp91@gmail.com>
2024-11-07 16:45:41 +0100

    fix: Use api key instead of jose signing for stitcher

--- packages/api/client/index.ts
+++ packages/api/client/index.ts
@@ -1,5 +1,4 @@
 import { treaty } from "@elysiajs/eden";
-import { SignJWT } from "jose";
 import type { App } from "../src";
 
 // API types are public by definition, we'll re-export them all.
@@ -9,36 +8,20 @@ export type ApiClient = ReturnType<typeof createApiClient>;
 
 interface CreateApiClientOptions {
   token?: string | null;
-  service?: {
-    name: string;
-    secret: string;
-  };
+  apiKey?: string | null;
 }
 
 export function createApiClient(
   domain: string,
   options?: CreateApiClientOptions,
 ) {
-  let token = options?.token;
-
-  if (options?.service) {
-    const jwt = new SignJWT({
-      type: "service",
-      name: options.service.name,
-    }).setProtectedHeader({ alg: "HS256" });
-
-    const key = new TextEncoder().encode(options.service.secret);
-
-    jwt.sign(key).then((value) => {
-      token = value;
-    });
-  }
-
   return treaty<App>(domain, {
     headers: () => {
       const headers: Record<string, string> = {};
-      if (token) {
-        headers["Authorization"] = `Bearer ${token}`;
+      if (options?.token) {
+        headers["Authorization"] = `Bearer ${options.token}`;
+      } else if (options?.apiKey) {
+        headers["x-api-key"] = options.apiKey;
       }
       return headers;
     },
--- packages/stitcher/src/lib/api-client.ts
+++ packages/stitcher/src/lib/api-client.ts
@@ -2,10 +2,5 @@ import { createApiClient } from "@superstreamer/api/client";
 import { env } from "../env";
 
 export const api = createApiClient(env.PUBLIC_API_ENDPOINT, {
-  service: env.SUPER_SECRET
-    ? {
-        name: "stitcher",
-        secret: env.SUPER_SECRET,
-      }
-    : undefined,
+  apiKey: env.SUPER_SECRET,
 });
