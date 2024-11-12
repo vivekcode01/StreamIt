c254f7e1d127a114250ea1ef44081cd2652636a5
Matthias Van Parijs <matvp91@gmail.com>
2024-11-13 00:05:13 +0100

    Added debugger

--- packages/api/scripts/dev.ts
+++ packages/api/scripts/dev.ts
@@ -1,3 +1,6 @@
 import { $ } from "bun";
 
-await Promise.all([$`tsup`, $`bun --watch ./src/index.ts`]);
+await Promise.all([
+  $`tsup`,
+  $`bun --watch --inspect=ws://localhost:6499/sprs-api ./src/index.ts`,
+]);
