5349a73c67e26bf2432ab77378efc9777bd59353
Matthias Van Parijs <matvp91@gmail.com>
2024-12-05 11:00:52 +0100

    fix: Exclude building player build before test

--- scripts/devtools/client-packages.ts
+++ scripts/devtools/client-packages.ts
@@ -1,10 +1,21 @@
 import { $ } from "bun";
 
-export async function buildClientPackages() {
-  await Promise.all([
+export async function buildClientPackages(options?: {
+  exclude: string[]
+}) {
+  let packageNames = [
     // Build api for the api client.
-    $`bun run --filter="@superstreamer/api" build`,
-    // Build player for app.
-    $`bun run --filter="@superstreamer/player" build`,
-  ]);
+    "@superstreamer/api",
+     // Build player for app.
+    "@superstreamer/player",
+  ];
+
+  if (options?.exclude) {
+    packageNames = packageNames.filter(name => !options.exclude.includes(name))
+  }
+
+  await Promise.all(packageNames.map(name => {
+    return $`bun run --filter="${name}" build`;
+  }));
 }
+
