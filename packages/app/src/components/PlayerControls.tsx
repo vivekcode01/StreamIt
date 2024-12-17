62a4146cd602b30f6bd324e45162ee4c5bd1d817
Matthias Van Parijs <matvp91@gmail.com>
2024-12-17 19:05:45 +0100

    Show cuepoints

--- packages/app/src/components/PlayerControls.tsx
+++ packages/app/src/components/PlayerControls.tsx
@@ -17,7 +17,7 @@ function Controls() {
     return null;
   }
   return (
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-2 overflow-hidden">
       <Timing />
       <Seekbar />
       <CuePoints />
