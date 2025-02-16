424b0d323dcedb1a002a80f2f7dd18346fa6a82c
Matthias Van Parijs <matvp91@gmail.com>
2025-02-16 17:37:21 +0100

    Env example files

--- scripts/lint.ts
+++ scripts/lint.ts
@@ -1,5 +1,5 @@
 import { $ } from "bun";
-import { buildClientPackages } from "./helpers";
+import { buildClientPackages } from "./misc/helpers";
 
 await buildClientPackages();
 
--- scripts/test.ts
+++ scripts/test.ts
@@ -1,5 +1,5 @@
 import { $ } from "bun";
-import { buildClientPackages } from "./helpers";
+import { buildClientPackages } from "./misc/helpers";
 
 await buildClientPackages({
   // We don't need the player package as we do not use it in tests.
