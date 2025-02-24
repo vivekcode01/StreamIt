4006fbd39ed06dfc2091b08d0e03e7cbe1326e54
Matthias Van Parijs <matvp91@gmail.com>
2025-02-24 14:43:58 +0100

    Added scroll

--- docs/.vitepress/theme/components/Scalar.vue
+++ docs/.vitepress/theme/components/Scalar.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
-import { ref, effect } from "vue";
+import { ref, effect, onMounted } from "vue";
 import { useData } from "vitepress";
 
 const { frontmatter } = useData();
 const iframeRef = ref();
+
+onMounted(() => {
+  window.addEventListener("message", (event) => {
+    let message;
+    try {
+      message = JSON.parse(event.data);
+    } catch {}
+
+    if (message?.type === "scalar:height" && iframeRef.value) {
+      iframeRef.value.style.height = `${message.height}px`;
+    }
+  });
+});
 
 effect(() => {
   if (!iframeRef.value) {
@@ -12,10 +25,11 @@ effect(() => {
 
   const params = {
     url: frontmatter.value.spec,
+    noScroll: true,
     config: {},
   };
 
-  const url = `https://superstreamer.xyz/scalar.html?params=${encodeURIComponent(JSON.stringify(params))}`;
+  const url = `/scalar.html?params=${encodeURIComponent(JSON.stringify(params))}`;
   iframeRef.value.src = url;
 });
 </script>
@@ -25,6 +39,7 @@ iframe {
   border: none;
   width: 100%;
   height: calc(100vh - var(--vp-nav-height));
+  max-width: 1140px;
 }
 </style>
 
