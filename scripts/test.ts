f3b9f91d7680d9877cd2bf4acad09368fbaea167
Matthias Van Parijs <matvp91@gmail.com>
2025-04-23 08:16:37 +0200

    chore: Removed unused code

--- scripts/test.ts
+++
@@ -1,9 +0,0 @@
-import { $ } from "bun";
-import { buildClientPackages } from "./misc/helpers";
-
-await buildClientPackages({
-  // We don't need the player package as we do not use it in tests.
-  exclude: ["@superstreamer/player"],
-});
-
-await $`bun run --filter="*" test`;
